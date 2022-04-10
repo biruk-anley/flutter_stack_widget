b1d07028ba130f4efb4ca6372da180f172c4c7aa
biruk <birukanley2022@gmail.com>
2022-04-10 22:43:46 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        title: 'GeeksforGeeks',
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Stack widget'),
+            centerTitle: true,
+            elevation: 12.0,
+          ),
+
+          body: Text("hellow"),
+        )),
+  );
+}
