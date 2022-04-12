bc4b937a920fe03fdcf9b072fb3873a90a1746e3
biruk <birukanley2022@gmail.com>
2022-04-12 23:26:54 +0300

    stack widget layout

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,45 @@ void main() {
         body: Center(
           child: Stack(
             children: <Widget>[
+              
               Container(
                 width: 350,
-                padding: EdgeInsets.fromLTRB(1, 20, 1, 20),
-                height: 250,
-                color:Color.fromARGB(255, 152, 152, 152),
-                child: CircleAvatar(
-                    backgroundImage: AssetImage('imgaes/me.png'),
-                    radius: 100,
+                 padding: EdgeInsets.all(5),
+                margin: EdgeInsets.all(5),
+                height: 350,
+                color:Color.fromARGB(175, 241, 235, 235),
+                
+                child: Padding(
+                  padding: EdgeInsets.all(45),
+                  child: CircleAvatar(  
+                  
+                    backgroundImage: AssetImage('assets/me2.png'),
+                    radius:100,
                     
                   ),
+                  )
+               
 
               ),
+              Positioned(
+                top: 220,
+                left:190,
+                child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 1),
+                        color: Color.fromARGB(159, 18, 18, 25)),
+                        child:Padding(
+                          padding:EdgeInsets.all(5),
+                          child: Text("Kibur",
+                          style: TextStyle(
+                          fontSize: 18,
+                          color: Color.fromARGB(255, 218, 213, 213),
+                    )
+                    
+                    ,),)
+                    
+                  ), )
+              
               
             ],
             
