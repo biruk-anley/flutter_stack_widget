9e94f043d609295b00f5c26d6125cb654b85d318
biruk <birukanley2022@gmail.com>
2022-04-16 00:58:13 +0300

    Grid widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:stack_images/pageTwo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,10 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: homepage(),
-      routes: {
-        
-        '/next': (context) => PageTwo(),
-      },
     );
   }
 }
@@ -36,50 +31,93 @@ class homepage extends StatelessWidget {
         centerTitle: true,
         elevation: 12.0,
       ),
-      body: Center(
-        child: Stack(
-          children: <Widget>[
-            Container(
-                width: 350,
-                padding: EdgeInsets.all(5),
-                margin: EdgeInsets.all(5),
-                height: 350,
-                color: Color.fromARGB(175, 241, 235, 235),
-                child: Padding(
-                  padding: EdgeInsets.all(45),
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/one.jpg'),
-                    radius: 100,
-                  ),
-                )),
-            Positioned(
-              top: 220,
-              left: 190,
-              child: Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 1),
-                      color: Color.fromARGB(159, 18, 18, 25)),
-                  child: Padding(
-                    padding: EdgeInsets.all(5),
-                    child: Text(
-                      "Kibur",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Color.fromARGB(255, 218, 213, 213),
-                      ),
-                    ),
-                  )),
-            )
-          ],
-        ),
+      body: Container(  
+            padding: EdgeInsets.all(12.0),  
+            child: GridView( 
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(  
+                  crossAxisCount: 2,  
+                  crossAxisSpacing: 4.0,  
+                  mainAxisSpacing: 4.0  
+              ),  
+        children: <Widget>[
+          Container(
+            child: Stack(
+             children: <Widget>[
+               Image(image: AssetImage('assets/one.jpg')),
+               Container(
+                 child: Column(
+                   mainAxisAlignment: MainAxisAlignment.end,
+                   children: <Widget>[
+                     Container(
+                       height: 80,
+                       width: 450,
+                       decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Color.fromARGB(90, 46, 39, 39),
+                                      width: 1),
+                                  color: Color.fromARGB(102, 37, 37, 45)),
+                       child: Row(
+                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                         crossAxisAlignment: CrossAxisAlignment.stretch,
+                         children: <Widget>[
+                           Column(
+                             mainAxisAlignment: MainAxisAlignment.center,
+                             children: <Widget>[
+                               Container(
+                                 margin: EdgeInsets.all(2),
+                                 child:Text('4 killo',
+                                 style: TextStyle(fontSize: 22, color: Color.fromARGB(228, 247, 245, 245)),
+                                 
+                                 
+                                 ),
+                                 ),
+                               Container(
+                                  margin: EdgeInsets.all(5),
+                                 child: Text("Addis Ababa",
+                                 style: TextStyle(
+                                              fontSize: 14,
+                                              color: Color.fromARGB(
+                                                  228, 247, 245, 245)),
+                                 ),
+                                 
+                               )
+                               
+                               
+                             ],
+                           ),
+                           
+                           Column(
+                             mainAxisAlignment: MainAxisAlignment.center,
+                             children: <Widget>[
+                               Container(
+                                 margin: EdgeInsets.all(15),
+                                 child: Icon(
+                                         Icons.star_border_purple500_outlined,
+                                         color: Color.fromARGB(255, 255, 255, 255),),
+                                       
+                                  )
+                               
+                             ],
+                             
+                           ) 
+                         ],
+                       ),
+                     )
+                   ],
+                 ),
+               )
+             ], 
+            ),
+            color: Colors.amber),
+          Container(color: Color.fromARGB(255, 160, 156, 145)),
+          Container(color: Color.fromARGB(255, 0, 0, 0)),
+          Container(color: Color.fromARGB(255, 7, 255, 106)),
+          Container(color: Color.fromARGB(255, 255, 7, 210)),
+          Container(color: Color.fromARGB(255, 255, 44, 7)),
+          Container(color: Color.fromARGB(255, 29, 29, 28)),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.arrow_forward_ios_rounded),
-        backgroundColor: Colors.blue,
-        onPressed: () {
-          Navigator.pushNamed(context, '/next');
-        },
-      ),
+      )
     );
   }
 }
